8a20a7008acbf65e2d933fd1f9aabf6021c2238a
Yanhui Shen <shen.elf@gmail.com>
2017-05-26 09:47:51 +0800

    Minor improvement

--- test/test.js
+++ test/test.js
@@ -1,42 +1,57 @@
 const assert = require('chai').assert;
 const Queue = require('../src/index.js');
 
-const LENGTH = 999;
+const LENGTH = 100;
 
 describe('Queue', () => {
-    let queue = new Queue();
 
-    it('should push', () => {
-        for (let i = 0; i < LENGTH; ++i) {
-            queue.push(i);
-        }
-        assert.equal(LENGTH, queue.length);
-    });
-
-    it('should unshift', () => {
-        for (let i = 0; i < LENGTH; ++i) {
-            queue.unshift(i);
-        }
-        assert.equal(LENGTH * 2, queue.length);
-    });
-
-    it('should shift', () => {
-        for (let i = LENGTH - 1; i >= 0; --i) {
-            let data = queue.shift();
-            assert.equal(i, data);
-        }
-        for (let i = 0; i < LENGTH; ++i) {
-            let data = queue.shift();
-            assert.equal(i, data);
-        }
-    });
-
-    it('should not shift if queue is empty', () => {
-        let data;
-        assert.doesNotThrow(() => {
-            data = queue.shift();
+    describe('#shift()', () => {
+        let queue = new Queue();
+        it('should not shift if queue is empty', () => {
+            let data;
+            assert.doesNotThrow(() => {
+                data = queue.shift();
+            });
+            assert.equal(0, queue.length);
+            assert.equal(null, data);
         });
-        assert.equal(0, queue.length);
-        assert.equal(null, data);
+    });
+
+    describe('#push()', () => {
+        let queue = new Queue();
+
+        it('should push', () => {
+            for (let i = 0; i < LENGTH; ++i) {
+                queue.push(i);
+                assert.equal(i + 1, queue.length);
+            }
+        });
+
+        it('should shift', () => {
+            for (let i = 0; i < LENGTH; ++i) {
+                let data = queue.shift();
+                assert.equal(i, data);
+                assert.equal(LENGTH - i - 1, queue.length);
+            }
+        });
+    });
+
+    describe('#unshift()', () => {
+        let queue = new Queue();
+
+        it('should unshift', () => {
+            for (let i = 0; i < LENGTH; ++i) {
+                queue.unshift(i);
+                assert.equal(i + 1, queue.length);
+            }
+        });
+
+        it('should shift', () => {
+            for (let i = 0; i < LENGTH; ++i) {
+                let data = queue.shift();
+                assert.equal(LENGTH - i - 1, data);
+                assert.equal(LENGTH - i - 1, queue.length);
+            }
+        });
     });
 });
